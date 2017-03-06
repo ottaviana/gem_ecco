a02c22669f612b431c6b116fbf48cd95df8159df
ottaviana <ottavianadavenia@gmail.com>
2017-03-06 11:37:56 +0100

    Lemonde bug fixed

--- lib/ecco/adapters/lemonde.rb
+++ lib/ecco/adapters/lemonde.rb
@@ -3,6 +3,7 @@ module Ecco
     def initialize
       @root_uri = 'http://www.lemonde.fr'
       @article_block = get_html(root_uri).css('.titre_une')
+      binding.pry
       @article_url = "http://www.lemonde.fr" + article_block.css('a').first.attr('href')
       @full_article_page = get_html(article_url)
     end
@@ -10,7 +11,7 @@ module Ecco
     def data
       SiteData.new(
         newspaper: "Le Monde",
-        headline: article_block.css('.tt3').first.children[0].text,
+        headline: article_block.css('a > h1 > text()').first,
         description: article_block.css('.description').inner_text,
         link: URI.parse(root_uri + article_block.css('a').first.attr('href')),
         image_url: URI.parse(article_block.css('img').first.attr('src')),
