491e5ee497273ed527712303d2c56e872d7e67ef
ottaviana <ottavianadavenia@gmail.com>
2017-05-14 21:18:54 +0200

    forbesus adapter bug related to the private variable solved

--- lib/ecco/adapters/forbesus.rb
+++ lib/ecco/adapters/forbesus.rb
@@ -6,7 +6,7 @@ module Ecco
       @article_block = get_html(root_uri).css("head > script")[4].text
       @article_json = article_block[39..-63]
       @object = JSON.parse(@article_json)
-      @article_object = object["homepage-usa_58d4014b6b1c8"]["contentPositions"][0]
+      @article_object = @object["homepage-usa_58d4014b6b1c8"]["contentPositions"][0]
       @full_article_page = get_html(article_url)
         #full article to do scraping json
     end
@@ -14,10 +14,10 @@ module Ecco
     def data
       SiteData.new(
         newspaper: "Forbes US",
-        headline: article_object["title"],
-        description: article_object["description"],
-        link: URI.parse(article_object["uri"]),
-        image_url: URI.parse(article_object["image"]),
+        headline: @article_object["title"],
+        description: @article_object["description"],
+        link: URI.parse(@article_object["uri"]),
+        image_url: URI.parse(@article_object["image"]),
         article: full_article_page.css(".content__article-body").text
       )
     end
