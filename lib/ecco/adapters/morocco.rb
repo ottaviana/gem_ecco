424e13bb8ff82cca6f8bf13df353124da1df9adb
ottaviana <ottavianadavenia@gmail.com>
2017-06-01 11:13:09 +0200

    morocco adapter fixed

--- lib/ecco/adapters/morocco.rb
+++ lib/ecco/adapters/morocco.rb
@@ -3,8 +3,8 @@ module Ecco
 
     def initialize
       @root_uri = 'https://www.moroccoworldnews.com/'
-      @article_block = get_html(root_uri).css('.td-module-thumb')
-      @article_url =  article_url =  article_block.css('a')[1].attr('href')
+      @article_block = get_html(root_uri).css('.td-big-grid-wrapper')
+      @article_url =  article_block.css('a')[1].attr('href')
       @full_article_page = get_html(article_url)
     end
 
@@ -14,7 +14,7 @@ module Ecco
         headline: article_block.css('a')[1].attr('title'),
         description: full_article_page.css('.m_8015943910318745746gmail-MsoNoSpacing')[0].text,
         link: URI.parse(article_url),
-        image_url: URI.parse(article_block.css('img')[1].attr('src')),
+        image_url: URI.parse(article_block.css('img')[0].attr('src')),
         article: full_article_page.css("div > p").text
       )
     end
